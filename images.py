b42433da198d3f60eba8ea88ef88fa5717911d38
pranav-sonawane <51134267+pranav-sonawane@users.noreply.github.com>
2020-05-16 01:28:31 +0530

    Improvements to Webscrapper

--- images.py
+++ images.py
@@ -2,16 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import PIL
 from io import BytesIO
+import os
 
-search = input("Search For:")
-params = {"q": search}
-r = requests.get("http://www.bing.com/images/search", params=params)
+def StartSearch():
+    search = input("Search For:")
+    params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
 
-soup = BeautifulSoup(r.text,"html.parser")
-links = soup.findAll("a",{"class": "thumb"})
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
 
-for item in links:
-    img_obj = requests.get(item.attrs["href"])
-    title = item.attrs["href"].split("/")[-1]
-    img = PIL.Image.open(BytesIO(img_obj.content))
-    img.save("./scraped_images/" +title, img.format)
+    r = requests.get("http://www.bing.com/images/search", params=params)
+
+    soup = BeautifulSoup(r.text,"html.parser")
+    links = soup.findAll("a",{"class": "thumb"})
+
+    for item in links:
+        img_obj = requests.get(item.attrs["href"])
+        print("Getting", item.attrs["href"])
+        title = item.attrs["href"].split("/")[-1]
+        try:
+            img = PIL.Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" +title, img.format)
+        except:
+            print("Could not save Image.")
+
+    StartSearch()
+
+StartSearch()
